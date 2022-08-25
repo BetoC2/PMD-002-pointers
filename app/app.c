1f7c5b6c35f83aef527049f589beca2d11f134b6
BetoC2 <renteria.alberto.c@gmail.com>
2022-08-24 19:11:03 -0500

    Se implemento la funcion calulate_frequency

--- app/app.c
+++ app/app.c
@@ -1,4 +1,16 @@
 int main(void)
 {
+    int array[10] = {7,5,5,5,5,1,4,3,6,8};
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ",array[i]);
+    }
+
+    int x = calulate_frequency(&array, 10, 5);
+
+    printf("\nVeces que se encontro %d en el array: %d\n", 5, x);
+
+
     return 0;
 }
